37366787d4248492da9c860735d69bc3ea81c82c
e_oneg1n <elobir@gmail.com>
2019-04-16 22:00:21 +0300

    Cleaned up. Added title, footer to the LaunchScreen

---
+++ MostActivePosts/AppDelegate.swift
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.swift
+//  MostActivePosts
+//
+//  Created by Eugene Lobyr on 4/16/19.
+//  Copyright © 2019 Eugene Lobyr. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        
+        return true
+    }
+
+}
+
